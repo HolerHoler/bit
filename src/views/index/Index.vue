c171f4c945f1f6959e9f3f1136a4e0f4e1e886e3
HolerHoler <holerholer41@gmail.com>
2019-12-06 00:20:11 +0800

    20191206 0020

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -22,63 +22,63 @@
 </template>
 
 <script>
-  import header from "@/components/header/header.vue";
-  import footer from "@/components/footer/footer.vue";
-  // import index from "@/components/index/index.vue";
-  //import SubMenu from "@/views/index/SubMenu";
+import header from "@/components/header/header.vue";
+import footer from "@/components/footer/footer.vue";
+// import index from "@/components/index/index.vue";
+//import SubMenu from "@/views/index/SubMenu";
 
-  export default {
-    name: "Index",
-    components: {
-      // "sub-menu": SubMenu
-      "index-header": header,
-      "index-footer": footer
-      // "index-content": index
-    },
-    data() {
-      return {};
-    },
-    created() {},
-    methods: {}
-  };
+export default {
+  name: "Index",
+  components: {
+    // "sub-menu": SubMenu
+    "index-header": header,
+    "index-footer": footer
+    // "index-content": index
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  #components-layout-demo-basic {
-    /* text-align: center; */
-  }
+#components-layout-demo-basic {
+  /* text-align: center; */
+}
 
-  #components-layout-demo-basic .ant-layout-header,
-  #components-layout-demo-basic .ant-layout-footer {
-    background: #7dbcea;
-    color: #fff;
-  }
+#components-layout-demo-basic .ant-layout-header,
+#components-layout-demo-basic .ant-layout-footer {
+  background: #7dbcea;
+  color: #fff;
+}
 
-  #components-layout-demo-basic .ant-layout-footer {
-    line-height: 1.5;
-  }
+#components-layout-demo-basic .ant-layout-footer {
+  line-height: 1.5;
+}
 
-  #components-layout-demo-basic .ant-layout-sider {
-    background: #3ba0e9;
-    color: #fff;
-    line-height: 120px;
-  }
+#components-layout-demo-basic .ant-layout-sider {
+  background: #3ba0e9;
+  color: #fff;
+  line-height: 120px;
+}
 
-  #components-layout-demo-basic .ant-layout-content {
-    /* background: rgba(16, 142, 233, 1);*/
-    /* color: #fff;  */
-    min-height: 600px;
-    line-height: 600px;
-  }
+#components-layout-demo-basic .ant-layout-content {
+  background: rgb(255, 255, 255);
+  /* color: #fff;  */
+  min-height: 600px;
+  line-height: 1.5;
+}
 
-  #components-layout-demo-basic>.ant-layout {
-    margin-bottom: 48px;
-  }
+#components-layout-demo-basic > .ant-layout {
+  margin-bottom: 48px;
+}
 
-  #components-layout-demo-basic>.ant-layout:last-child {
-    margin: 0;
-  }
+#components-layout-demo-basic > .ant-layout:last-child {
+  margin: 0;
+}
 </style>
 
 
